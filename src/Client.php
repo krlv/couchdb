a4bb950801e68d93eac82cb22082ed5109b664b1
krlv <eug.krlv@gmail.com>
2018-10-27 08:59:58 -0700

    Initial commit

---
+++ src/Client.php
@@ -0,0 +1,9 @@
+<?php
+namespace Couchdb;
+
+class Client
+{
+    public function __construct()
+    {
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Couchdb\Test;
+
+class ClientTest extends \PHPUnit\Framework\TestCase
+{
+    public function testConstructor()
+    {
+        $this->assertTrue(true);
+    }
+}
